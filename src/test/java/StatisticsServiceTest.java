f79d35553a1716b1c5d7734be771beea56b57ccb
Nikita Samoilov <nikisamoylov@gmail.com>
2022-12-30 12:03:30 +0400

    add third test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -1,6 +1,5 @@
-package ru.netology.statistic;
-
 import org.junit.jupiter.api.Test;
+import ru.netology.statistic.StatisticsService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +11,30 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void findMaxsecondopt() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 34, 8, 6, 11, 11, 12};
+        long expected = 34;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void findMaxthirdopt() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 34, 8, 6, 11, 11, 56};
+        long expected = 56;
 
         long actual = service.findMax(incomesInBillions);
 
